2c337b30e98c44622af2fb7c040ce4ee9ef77066
Carlos V <cavarod@gmail.com>
2021-03-29 11:14:10 -0700

    finished the project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Adding Functions to our required set 
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site');
 const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
@@ -146,12 +146,18 @@ Add a New Project
 promptUser()
 	.then(promptProject)
 	.then(portfolioData => {
-
-		const pageHTML = generatePage(portfolioData);
-
-		fs.writeFile('./index.html', pageHTML, err => {
-		  if (err) throw err;
-
-		  console.log('Portfolio complete! Check out index.html to see the output!');
-		});
+	return generatePage(portfolioData);
+	})
+	.then(pageHTML => {
+	return writeFile(pageHTML);
+	})
+	.then(writeFileResponse => {
+	console.log(writeFileResponse);
+	return copyFile();
+	})
+	.then(copyFileResponse => {
+	console.log(copyFileResponse);
+	})
+	.catch(err => {
+	console.log(err);
 	});
